9c0d1342f540a0785a054fb085828789e17621e9
ePubRepo <epubrepo@epubrepo.com>
2014-07-23 21:06:29 -0400

    lint

--- src/practice_questions/Main.java
+++ src/practice_questions/Main.java
@@ -1,22 +1,20 @@
 package practice_questions;
 
 public class Main {
+  public static void main(String[] args) {
+    SinglyLinkedListNode_1342<Integer> node1 = new SinglyLinkedListNode_1342<Integer>(1);
+    SinglyLinkedListNode_1342<Integer> node2 = new SinglyLinkedListNode_1342<Integer>(2);
+    SinglyLinkedListNode_1342<Integer> node3 = new SinglyLinkedListNode_1342<Integer>(3);
+    SinglyLinkedListNode_1342<Integer> node4 = new SinglyLinkedListNode_1342<Integer>(4);
+    SinglyLinkedListNode_1342<Integer> node5 = new SinglyLinkedListNode_1342<Integer>(5);
 
-	public static void main(String[] args) {
-        SinglyLinkedListNode_1342<Integer> node1 = new SinglyLinkedListNode_1342<Integer>(1);
-		SinglyLinkedListNode_1342<Integer> node2 = new SinglyLinkedListNode_1342<Integer>(2);
-		SinglyLinkedListNode_1342<Integer> node3 = new SinglyLinkedListNode_1342<Integer>(3);
-		SinglyLinkedListNode_1342<Integer> node4 = new SinglyLinkedListNode_1342<Integer>(4);
-		SinglyLinkedListNode_1342<Integer> node5 = new SinglyLinkedListNode_1342<Integer>(5);
-		
-		node1.setNextNode(node2);
-		node2.setNextNode(node3);
-		node3.setNextNode(node4);
-		node4.setNextNode(node5);
-		
-		SinglyLinkedListHelper_1342<Integer> helper = new SinglyLinkedListHelper_1342<Integer>(node1);
-		SinglyLinkedListNode_1342<Integer> val = helper.findMthToLastNode(4);
-		System.out.println(val.getValue());
-	}
+    node1.setNextNode(node2);
+    node2.setNextNode(node3);
+    node3.setNextNode(node4);
+    node4.setNextNode(node5);
 
+    SinglyLinkedListHelper_1342<Integer> helper = new SinglyLinkedListHelper_1342<Integer>(node1);
+    SinglyLinkedListNode_1342<Integer> val = helper.findMthToLastNode(4);
+    System.out.println(val.getValue());
+  }
 }
