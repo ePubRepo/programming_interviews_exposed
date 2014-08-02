f5c04a4c9651538c955ef8fbe9639dcb1927939e
ePubRepo <ePubRepo@ePubRepo.com>
2014-08-02 08:27:39 -0400

    Fixing corner cases

--- src/practice_questions/SnakeSequence.java
+++ src/practice_questions/SnakeSequence.java
@@ -6,26 +6,28 @@ public class SnakeSequence {
   private final int maxX = 3;
   private final int maxY = 3;
   private final int[][] board = new int[][]{
-      {1, 3, 2, 4},
+      {1, 3, 0, 4},
       {2, 1, -1, 4},
-      {1, 2, -1, 0},
-      {2, 0, 1, -1}
+      {3, 0, -1, 0},
+      {6, 2, 1, -1}
   };
 
   private ArrayList<Integer> findSequence(int xPos,
       int yPos, ArrayList<Integer> currentPath) {
-    currentPath.add(board[xPos][yPos]);
+    currentPath.add(board[yPos][xPos]);
 
-    ArrayList<Integer> pathRight = currentPath;
-    ArrayList<Integer> pathDown = currentPath;
+    ArrayList<Integer> pathRight = new ArrayList<Integer>(currentPath);
+    ArrayList<Integer> pathDown = new ArrayList<Integer>(currentPath);
 
-    if (xPos < maxX && yPos < maxY) {
-      if (board[xPos][yPos + 1] + 1 == board[xPos][yPos] ||
-          board[xPos][yPos + 1] - 1 == board[xPos][yPos]) {
+    if (xPos < maxX || yPos < maxY) {
+      if (yPos < maxY &&
+          (board[yPos + 1][xPos] + 1 == board[yPos][xPos] ||
+          board[yPos + 1][xPos] - 1 == board[yPos][xPos])) {
         pathDown = findSequence(xPos, yPos + 1, currentPath);
       }
-      if (board[xPos + 1][yPos] + 1 == board[xPos][yPos] ||
-          board[xPos + 1][yPos] - 1 == board[xPos][yPos]) {
+      if (xPos < maxX &&
+          (board[yPos][xPos + 1] + 1 == board[yPos][xPos] ||
+          board[yPos][xPos + 1] - 1 == board[yPos][xPos])) {
         pathRight = findSequence(xPos + 1, yPos, currentPath);
       }
 
@@ -46,7 +48,6 @@ public class SnakeSequence {
     for (int i = 0; i < result.size(); i++) {
       System.out.println(result.get(i));
     }
-    
   }
 
   public static void main(String[] args) {
